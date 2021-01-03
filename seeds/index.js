a1bb2f4809310fef283cdd5d0592e6bfca6e3319
TaiyaKIE <46888320+book9222@users.noreply.github.com>
2021-01-03 22:27:02 +0700

    Add views-layout-partial, Update model

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 40) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/369',
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero, reprehenderit? Ad quae laborum labore sapiente deserunt officiis obcaecati natus culpa rerum eaque, harum incidunt sunt aliquam numquam quibusdam. Dolore, eos!',
+            price: price
         })
         await camp.save();
     }
